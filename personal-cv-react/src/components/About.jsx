f2227241c6e12317e4737115da2be5c22e9744a2
Allen Literato <Vanessa Dajang>
2026-03-04 17:15:05 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <h2>About Me</h2>
       <img src={profileImg} alt="Profile" style={{ width: '150px', borderRadius: '50%' }} />
       
-      <p>I am a hardworking student.</p>
+      <p>I am a good student.</p>
       <p>Email: <a href="mailto:vanessada758@gmail.com">vanessada758@gmail.com</a><br />
       GitHub: <a href="https://github.com/dajangvanessa-lab" target="_blank">github.com</a></p>
     </section>
